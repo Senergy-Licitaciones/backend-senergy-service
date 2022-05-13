0c2f02a7d0654e1d8431a3d30b6f29c60c85a894
Marco <marco.fura@unmsm.edu.pe>
2022-05-13 17:20:48 -0500

    auth role solved

--- middlewares/roleAuth.js
+++ middlewares/roleAuth.js
@@ -2,8 +2,8 @@ const checkRoleAuth=(roles)=>async(req,res,next)=>{
     try{
         const proveedor=req.proveedor;
         const user=req.user;
-        if(proveedor)[].concat(roles).includes(user.role)?next():res.status(409).send({message:"Usuario sin permisos",error:true});
-        if(user)[].concat(roles).includes(proveedor.role)?next():res.status(409).send({message:"Proveedor sin permisos",error:true});
+        if(proveedor)[].concat(roles).includes(proveedor.role)?next():res.status(409).send({message:"Usuario sin permisos",error:true});
+        if(user)[].concat(roles).includes(user.role)?next():res.status(409).send({message:"Proveedor sin permisos",error:true});
     }catch(err){
         return res.status(500).send({
             message:"Ha ocurrido un error en la autenticación",
