44f5a11518a6a4dc4073e0af6c46e32bb35bda74
Marco <marco.fura@unmsm.edu.pe>
2022-06-11 09:33:41 -0500

    more logs added

--- src/middlewares/checkUserType.ts
+++ src/middlewares/checkUserType.ts
@@ -6,15 +6,19 @@ import UsuarioModel from "../apiServices/usuario/model";
 import ProveedorModel from "../apiServices/proveedor/model";
 const checkUserType:CheckUserType=(types)=>async(req,res,next)=>{
     try{
+        console.log("iniciando check user type");
         const auth=req.headers.authorization;
         if(!auth) throw new Error("Token no proporcionado");
+        console.log("spliteando token");
         const token=auth.split(" ").pop();
         if(!token)throw new Error("Token inválido");
+        console.log("verificando token");
         const tokenData=verifyToken(token);
         if(!tokenData) return res.status(400).send({
             error:true,
             message:"Token inválido"
         });
+        console.log("definiendo array");
         const initialArray:Type[]=[];
         console.log("type token ",tokenData.type," ",types.toString());
         if(initialArray.concat(types).includes(tokenData.type)){
@@ -24,19 +28,20 @@ const checkUserType:CheckUserType=(types)=>async(req,res,next)=>{
                 if(user.estado==="offline")return res.status(400).send({message:"Debe iniciar sesión",error:true})
                 req.user=user;
                 
-            }else{
+            }
+            if(tokenData.type===Type.Proveedor){
                 const proveedor=await ProveedorModel.findById(tokenData._id);
                 if(!proveedor) return res.status(400).send({message:"Usuario sin permisos",error:true})
                 req.proveedor=proveedor;
             }
             console.log("antes del next en user type");
             return next();
-        }else{
-            return res.status(400).send({
+        }
+        console.log("else antes del res status ");
+        return res.status(400).send({
                 error:true,
                 message:"No tiene permisos para realizar esta acción"
-            })
-        }
+        });
     }catch(err){
         console.log("error catch user type ",err);
         return res.status(500).send("Ha ocurrido un error en la autenticación");
